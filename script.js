488b1aa97533505ccfea8efe5cce4b96523c64ac
brunaportelaa <brunaporteladj@gmail.com>
2024-04-10 20:13:28 -0300

    Exercise for chapter 1, lesson 8 solved

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ info = {
     blog: 'https://raybo.org'
   }
 }
+const $ = document.querySelector.bind(document);
 
-function listObject(data) {}
+function listObject(data) {
+  const $ = document.querySelector.bind(document)
+  let output = '<ul>'
+  for (const key in data){
+    output += `<li><a href="${data[key]}">${key}</a></li>`;
+  }
+  output += '</ul>'
+  $('#output').innerHTML = output
+}
 
 listObject(info.social)
