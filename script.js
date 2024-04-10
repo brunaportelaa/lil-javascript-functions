aecd892ad917c64675f7dbc05b24c2c0cba239fe
brunaportelaa <brunaporteladj@gmail.com>
2024-04-10 20:54:06 -0300

    Exercise solved for chapter 2, lesson 1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function add(...theArgs) {
-  let argSum = theArgs.reduce((sum, val) => sum + val)
-  return argSum;
-}
+//Solution 1, more readable
+const add = (...theArgs) => theArgs.reduce((sum, val) => sum + val)
 output = add(1,2,3,4,5);
 console.log(output) //15
+
+//Solution 2, more compact but far less readable
+console.log([1,2,3,4,5].reduce((sum,val) => sum+val))
